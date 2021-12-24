1c2a533551a0d36493bd2a02cc22ad73448ba4cc
mohammadmiyan <mohammad.miyan@intigral.net>
2021-12-24 20:25:16 +0400

    merge two stream

--- src/com/example/mergeTwoMaps/MergeTwoStreamsTest.java
+++ src/com/example/mergeTwoMaps/MergeTwoStreamsTest.java
@@ -1,7 +1,18 @@
 package com.example.mergeTwoMaps;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MergeTwoStreamsTest {
     public static void main(String[] args) {
         System.out.println("hey");
+
+
+        List<String> list1 = Arrays.asList("Java", "8");
+        List<String> list2 = Arrays.asList("explained", "through", "programs");
+        List<String> collect = Stream.of(list1, list2).flatMap(list -> list.stream()).collect(Collectors.toList());
+        System.out.println(collect);
     }
 }
